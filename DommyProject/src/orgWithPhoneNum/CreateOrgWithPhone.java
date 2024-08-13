d11617bc09e0ae592cbd29a17acbac54e1ccf965
gangu <gangu@Abhishek>
2024-08-13 14:36:11 +0530

    9114268898 is added

--- DommyProject/src/orgWithPhoneNum/CreateOrgWithPhone.java
+++ DommyProject/src/orgWithPhoneNum/CreateOrgWithPhone.java
@@ -4,6 +4,8 @@ public class CreateOrgWithPhone {
 	
 	public static void main(String[] args) {
 		System.out.println("created org with phone number");
+		
+		System.out.println("created org with phone number 9114268898");
 	}
 
 }
